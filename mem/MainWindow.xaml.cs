5c67ad6717cd33380838f4322b5e41a60090a052
Алексей Фоменко <genius690@mail.ru>
2021-01-21 11:05:23 +0700

    f

---
+++ mem/MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using mem;
+using System.Xml.Serialization;
+
+namespace mem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        List<memeClass> memList = new List<memeClass>(); //список объектов типа мем
+
+        List<string> categories = new List<string>() {"any", "cat1", "cat2", "cat3", "cat4" }; // список категорий, можно менять, кол-во не важно, "any" рекомендую оставить
+        public MainWindow()
+        {
+            InitializeComponent();
+            mCategoryCb.ItemsSource = categories;//говорим листбоксу с категориями из какого списка брать категории
+            mList.DisplayMemberPath = "_mName"; // говорим списку с мемами, что нужно отображать не тип класса, а определенное поле из класса
+        }
+
+        private void mSearch_Click(object sender, RoutedEventArgs e) //поиск
+        {
+            mList.Items.Clear();
+           foreach (memeClass mTemp in memList) //каждый элемент типа мем в списке мемеов
+            {
+                if (mTemp.getName().Contains(mNameTb.Text)) //проверяем: если  имя экземпляра содержит текст из текстбокса, то добавляем этот экземпляр в листбокс
+                {
+                    mList.Items.Add(mTemp); // добавили
+                }
+
+            }
+        }
+
+        private void add_mems_Click(object sender, RoutedEventArgs e) //кнопка открытия формы добавления
+        {
+            add add = new add(memList.Count,categories); // создаем экземпляр формы "добавить", передаем в него количество мемов в списке и список категорий 
+            add.Show(); //выводим на экран форму
+            add_mems.IsEnabled = false; //выключаем кнопку добавления
+            add.Closing += Add_Closed; //создаем событие закрытие формы добавить, при закрытие будет вызываться метод add_closed, он ниже
+        }
+
+        private void Add_Closed(object sender, EventArgs e) 
+        {
+            
+            var s = sender as add; //тут мы создаем для удобсва переменную, сендер это и есть форма, нужно обозначить что она имеет тип добавить
+
+            if (s.GetMeme() != null)  //если функция получить мем сработала
+            {
+                memList.Add(s.GetMeme()); //добавляем мем в список мемеов
+
+                mList.Items.Add(memList.Last()); //добавляем последний мем из списка в листбокс
+            }
+            add_mems.IsEnabled = true; //включаем кнопку
+            
+            Closing -= Add_Closed; //отписуемся от события 
+        }
+
+        private void mList_SelectionChanged(object sender, SelectionChangedEventArgs e) //обработка выбора мемов в листбоксе
+        {
+            if (mList.SelectedItem != null) // если выбран не пустой элемент
+            {
+                memeTagsTb.Text = ""; 
+                memeClass temp = mList.SelectedItem as memeClass; //создаем переменную типа мем из выбранного элемента из листбокса с мемами
+                imageBox1.Source = temp.getFName(); //в имаджбокс пишем путь до картинки 
+                memeNameTb.Text = temp.getName() + " : " + temp.getId(); // в текстбокс имени пишем имя и id мема в списке (+ " : " + temp.getId() - эту часть можно удалить и айди не будет оборажаться)
+                foreach (string str in temp.getTags()) //получаем список тэгов 
+                    memeTagsTb.Text += str + ", "; //пишем тэги в бокс для тэгов
+                memeCategorTb.Text = temp.getCategory(); //вывод категории в тб категорий
+            }
+        }
+
+        private void mCategoryCb_SelectionChanged(object sender, SelectionChangedEventArgs e) //сортировка по категориям
+        {
+            if ((mCategoryCb.SelectedIndex != -1) && mCategoryCb.SelectedItem.ToString() != "any") // если выбранная категория не за гранницей и не равно любой 
+            {
+                mList.Items.Clear();
+                foreach (memeClass memSampl in memList) //проход по списку мемов
+                {
+                    if (memSampl.getCategory() == mCategoryCb.SelectedItem.ToString()) //сравнием категории мема и выбраннную
+                    {
+                        mList.Items.Add(memSampl); // добавляем если сошлось
+                    }
+                }
+            }
+            else  //если нет
+                if (mCategoryCb.SelectedItem.ToString() == "any")  //если категория любая 
+            {
+                    mList.Items.Clear();
+                    foreach (memeClass temp in memList) // выводим все мемы по очереди
+                    mList.Items.Add(temp);
+                }
+        }
+
+        private void removButton_Click(object sender, RoutedEventArgs e) // кнопка удаления
+        {
+            if (mList.SelectedItem != null) // если ывбран не пустой
+            {
+                memList.Remove(mList.SelectedItem as memeClass); // удаляем мем из списка мемов выбранный элемент из листбокса мемов, который приведен к типу мем
+
+                mList.Items.Remove(mList.SelectedItem); // удаление из листбокса мемов
+            }
+        }
+
+        private void tagsSearchBut_Click(object sender, RoutedEventArgs e) //поиск по тэгам
+        {
+            mList.Items.Clear();
+            List<string> sTgs = new List<string>(); //список тэгов создаем
+            string[] temp = tagsSearchTb.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); //из текстбокса введеные тэги через пробел делим на слова, пихаем их в массив слов темп
+            foreach (string sTemp in temp)
+            {
+                sTgs.Add(sTemp); //добавляем слова из массива в список
+            }
+
+            foreach (memeClass memSample in memList) // проходим список мемов
+            {
+                List<string> tagsList = memSample.getTags(); //получаем список тэгов мема
+                foreach (string tag in tagsList) // получаем конкретный тэг 
+                    foreach (string st in sTgs) //получаем тэг мема
+                        if (tag == st) // если тэг есть
+                        {
+                            mList.Items.Add(memSample);//добавляем мем в список
+                            break; //выходим из цикла чтоб не искать дальше
+                        }
+            }
+        }
+
+        private void savejson_Click(object sender, RoutedEventArgs e) //сохранение в джейсон файл 
+        {
+
+            saveXml(memList);
+        }
+
+        private void openJson_Click(object sender, RoutedEventArgs e) //открыть из джсон
+        {
+            mList.Items.Clear();
+            var t = openXml();
+            if(t != null)
+            foreach (memeClass temp in t)
+            {
+                mList.Items.Add(temp);
+            }
+
+        }
+
+        void saveXml (List<memeClass> mem)
+        {
+            string path = "";
+            SaveFileDialog openFileDialog = new SaveFileDialog(); 
+            openFileDialog.Filter = "Файлы xml (*.xml) | (*.xml;)"; 
+            var result = openFileDialog.ShowDialog(); 
+            if (result == true)
+            {
+               
+                path = openFileDialog.FileName; 
+                XmlSerializer serializer = new XmlSerializer(typeof(List<memeClass>));
+                using (FileStream f = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    serializer.Serialize(f, mem);
+                }
+            }
+            
+        }
+
+        List<memeClass> openXml ()
+        {
+            string path = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы xml (*.xml) | (*.xml;)";
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                path = openFileDialog.FileName;
+                XmlSerializer serializer = new XmlSerializer(typeof(List<memeClass>));
+                List<memeClass> temp;
+                using (FileStream f = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    temp = serializer.Deserialize(f) as List<memeClass>;
+                }
+                return temp;
+            }
+            else return null;
+        }
+    }
+}
---
+++ mem/add.xaml.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace mem
+{
+    /// <summary>
+    /// Логика взаимодействия для add.xaml
+    /// </summary>
+    public partial class add : Window
+    {
+        private string filename = "";//путь до файла
+
+        private string category = ""; //категория
+
+        private string name = ""; //имя файла
+
+        private List<string> tags = new List<string>(); //список тэгов
+
+        private memeClass mem; //экземпляр мем
+
+        private int mId = 0; //айди мема
+
+
+
+
+
+
+        private string openImag() //функция открытие картинки 
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // форма
+            openFileDialog.Filter = "Файлы изображений (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png)"; // филтр файлов изображения
+            var result = openFileDialog.ShowDialog(); // открытие формы
+            if (result == true)
+            {
+                mNameTB.Text = openFileDialog.SafeFileName; //в текстобокс вывод пути до файла
+                return openFileDialog.FileName; // вернуть путь до файла
+            }
+            else return null; //иначе ничего 
+        }
+
+        public memeClass GetMeme() => mem; //вернуть тип класса мем
+
+        public add(int id,List<string> surs) //конструктор формы добавить, передаем в эту форму из главной айди и список категорий
+        {
+            InitializeComponent();
+            mCategory.ItemsSource = surs; //категории из списка
+            this.mId = id; //назначаем айди
+            filepick.IsEnabled = false; //кнопка переключения файла \ ссылки выключена
+        }
+
+        private memeClass formMeme(string tags, string name, string category, string filename) // фукнция формирования мемов
+        { 
+            string[] temp = tags.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); //делим тэги на слова
+            foreach (string sTemp in temp)
+            {
+                this.tags.Add(sTemp); // пишем в список тэгов тэги
+            }
+
+            this.name = name; // пишем имя в имя
+
+            this.category = category; // категорию в категорию
+
+            return new memeClass(this.name, this.category, this.tags, this.filename, mId); // возвращаем новый экземпляр мема с параметрами
+        }
+
+        private void openIMG_Click(object sender, RoutedEventArgs e) //кнопка открытие изобр
+        {
+            filename = openImag(); // в путь до файла пишем из функции путь до файла
+
+            if (filename != null) //если путь не пустой
+            {
+
+                BitmapImage bitmap = new BitmapImage(); //новый экзмплр картинки
+                bitmap.BeginInit(); //начало редкатирования 
+                bitmap.UriSource = new Uri(filename); //путь до файла
+                bitmap.EndInit(); // конец редактирования
+
+                memPrev.Source = bitmap; //предпросмотр картинки
+
+                fnametb.Text = filename; //путь до файла 
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((mTagsTB.Text != "") && (mNameTB.Text != "") && (mCategory.SelectedIndex != -1) && (filename != null)) //если поля заполнены
+            {
+               mem = formMeme(mTagsTB.Text, mNameTB.Text, mCategory.SelectedItem.ToString(), filename); // в переменную мем записываем мем с параметрами из функции гетмем
+
+                this.Hide(); //прячем форму
+
+                this.Close(); //закрываем форму
+            }
+            else
+            {
+                warnings.Content = "некоторые поля пустые"; //если какие то поля пустые, предупреждаем
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) //событие закрытия формы
+        {
+            this.Hide(); //прячем форму
+        }
+
+
+        private void filepick_Click(object sender, RoutedEventArgs e) //переключаем кнопки 
+        {
+            filepick.IsEnabled = false;
+            urlpick.IsEnabled = true;
+            urlpath.Visibility = (Visibility)1; 
+            fnametb.Visibility = 0;
+            openIMG.Visibility = 0;
+        }
+
+        private void urlpick_Click(object sender, RoutedEventArgs e) //тоже самое ничего сложного
+        {
+            filepick.IsEnabled = true;
+            urlpick.IsEnabled = false;
+            urlpath.Visibility = 0;
+            fnametb.Visibility = (Visibility)1;
+            openIMG.Visibility = (Visibility)1;
+        }
+
+
+        private void urlpath_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) //текстбокс для юрл картинки
+        {
+            if (urlpath.Text != "") // если он не пустой 
+            {
+                try //блок защиты от НЕ ссылки
+                {
+                    BitmapImage bitmap = new BitmapImage();//экзплр картинки 
+                    bitmap.BeginInit();//начало
+                    bitmap.UriSource = new Uri(urlpath.Text);//путь
+                    bitmap.EndInit();//конец
+
+                    memPrev.Source = bitmap;//превью
+
+                    filename = urlpath.Text;//путь
+                }
+                catch (System.UriFormatException) //ловим ошибки если в тексбокс введен не юрл
+                {
+
+                }
+            }
+        }
+    }
+}
